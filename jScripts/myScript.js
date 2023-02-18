ec55b24958f5e83ff78f6a6ebedeefd93fbb755f
benmeir65 <107880688+benmeir65@users.noreply.github.com>
2023-02-19 00:10:19 +0200

    Add files via upload

---
+++ jScripts/myScript.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    $(".about").click(function () {
+        $("#aboutDiv").toggle();
+    });
+
+    $(".howToPlay").click(function () {
+        $("#howToPlayDiv").toggle();
+    });
+
+    $(".closeAbout").click(function () {
+        $("#aboutDiv").hide();
+        $("#gameIframe")[0].contentWindow.focus();
+    });
+
+    $(".closeHowToPlay").click(function () {
+        $("#howToPlayDiv").hide();
+        $("#gameIframe")[0].contentWindow.focus();
+    });
+
+    $(".Copyright").click(function () {
+        $("#copyrightDiv").toggle();
+    });
+
+    $(".closeCopyright").click(function () {
+        $("#copyrightDiv").hide();
+        $("#gameIframe")[0].contentWindow.focus();
+    });
+
+    });
